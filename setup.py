8eb0e0fd374ad26d2b6af6bf608a19f393b3541f
santosh <santosh4thmarch@gmail.com>
2025-11-18 17:55:55 +0530

    Project structure is done and local package src isntalled in our venv using -e . in the end of requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="santosh soni",
+    author_email="santosh4thmarch@gmail.com",
+    packages=find_packages()
+)
